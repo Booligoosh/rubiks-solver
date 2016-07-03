88ccbc290d9a00fbdfee0c24cbfd4230dcd73791
Booligoosh <booligoosh@optusnet.com.au>
2016-07-03 12:07:13 +1000

    gave corners positions
    fb => front/back
    tb => top/bottom
    lr => left/right

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ var fururf = [[0,true],[4,true],[3,true],[4,false],[3,false],[0,false]];
 //defs
 var colors = ["white", "yellow", "green", "blue", "red", "orange"];
 //pieces
-var corner1 = {tiles:3, tile1:2, tile2:3, tile3:1};
-var corner2 = {tiles:3, tile1:2, tile2:3, tile3:1};
-var corner3 = {tiles:3, tile1:2, tile2:3, tile3:1};
-var corner4 = {tiles:3, tile1:2, tile2:3, tile3:1};
-var corner5 = {tiles:3, tile1:2, tile2:3, tile3:1};
-var corner6 = {tiles:3, tile1:2, tile2:3, tile3:1};
-var corner7 = {tiles:3, tile1:2, tile2:3, tile3:1};
-var corner8 = {tiles:3, tile1:2, tile2:3, tile3:1};
+var corner1 = {tiles:3, tile1:2, tile2:3, tile3:1, fb:true, tb:true, lr:true};
+var corner2 = {tiles:3, tile1:2, tile2:3, tile3:1, fb:true, tb:true, lr:false};
+var corner3 = {tiles:3, tile1:2, tile2:3, tile3:1, fb:true, tb:false, lr:true};
+var corner4 = {tiles:3, tile1:2, tile2:3, tile3:1, fb:true, tb:false, lr:false};
+var corner5 = {tiles:3, tile1:2, tile2:3, tile3:1, fb:false, tb:true, lr:true};
+var corner6 = {tiles:3, tile1:2, tile2:3, tile3:1, fb:false, tb:true, lr:false};
+var corner7 = {tiles:3, tile1:2, tile2:3, tile3:1, fb:false, tb:false, lr:true};
+var corner8 = {tiles:3, tile1:2, tile2:3, tile3:1, fb:false, tb:false, lr:false};
 var edge1 = {tiles:2, tile1:2, tile2:5};
 var edge2 = {tiles:2, tile1:2, tile2:5};
 var edge3 = {tiles:2, tile1:2, tile2:5};
