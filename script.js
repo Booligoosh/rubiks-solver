18cf34bc3ee6a11a5e0696d2fb15c861394c69f7
Booligoosh <booligoosh@optusnet.com.au>
2016-07-03 11:58:36 +1000

    finished defining var examples (was interrupted)

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+//algorithms
+var fururf = [[0,true],[4,true],[3,true],[4,false],[3,false],[0,false]];
+//defs
 var colors = ["white", "yellow", "green", "blue", "red", "orange"];
+//pieces
 var corner1 = {tiles:3, tile1:2, tile2:3, tile3:1};
 var corner2 = {tiles:3, tile1:2, tile2:3, tile3:1};
 var corner3 = {tiles:3, tile1:2, tile2:3, tile3:1};
@@ -8,4 +12,20 @@ var corner6 = {tiles:3, tile1:2, tile2:3, tile3:1};
 var corner7 = {tiles:3, tile1:2, tile2:3, tile3:1};
 var corner8 = {tiles:3, tile1:2, tile2:3, tile3:1};
 var edge1 = {tiles:2, tile1:2, tile2:5};
-var edge1 = {tiles:2, tile1:2, tile2:5};
+var edge2 = {tiles:2, tile1:2, tile2:5};
+var edge3 = {tiles:2, tile1:2, tile2:5};
+var edge4 = {tiles:2, tile1:2, tile2:5};
+var edge5 = {tiles:2, tile1:2, tile2:5};
+var edge6 = {tiles:2, tile1:2, tile2:5};
+var edge7 = {tiles:2, tile1:2, tile2:5};
+var edge8 = {tiles:2, tile1:2, tile2:5};
+var edge9 = {tiles:2, tile1:2, tile2:5};
+var edge10 = {tiles:2, tile1:2, tile2:5};
+var edge11 = {tiles:2, tile1:2, tile2:5};
+var edge12 = {tiles:2, tile1:2, tile2:5};
+var center1 = {tiles:1, tile1:2};
+var center2 = {tiles:1, tile1:2};
+var center3 = {tiles:1, tile1:2};
+var center4 = {tiles:1, tile1:2};
+var center5 = {tiles:1, tile1:2};
+var center6 = {tiles:1, tile1:2};
