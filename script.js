3d651c11df5eec745819d905479ecd76c4f35aef
Booligoosh <booligoosh@optusnet.com.au>
2016-07-28 12:53:14 +1000

    added rotateCorner

--- script.js
+++ script.js
@@ -6,11 +6,21 @@ var colors = ["white", "yellow", "green", "blue", "red", "orange"];
 //pieces
 var pieces = [];
 for(var i = 0; i < (t*t*t) - (t - 2); i++) {
-    pieces.push({x: 0, y: 0, z: 0, tiles: [], type: 2});
+    pieces.push({x: 0, y: 0, z: 0, tiles: [2,3,6], type: 2});
 }
 
 function piece(n) {
     return(pieces[n]);
+}
+
+function rotateCorner(index,dir) {
+    if (dir === true) {
+        piece(index).tiles.splice(0,0,piece(index).tiles[piece(index).tiles.length - 1]);
+        piece(index).tiles.splice(piece(index).tiles.length - 1, 1);
+    }
+    if (dir === false) {
+        piece(index).tiles.push(piece(index).tiles.shift(1));
+    }
 }
 
 function doAlgorithm(array) {
