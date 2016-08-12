a9a5483dc61fadc852ee8079e3092b2659afec73
Booligoosh <booligoosh@optusnet.com.au>
2016-08-12 19:31:03 +1000

    switched to bands

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
+/*
+FACE NUMBERS
+-------------
+top face: 0
+bottom face: 1
+left face: 2
+right face: 3
+front face: 4
+back face: 5
+-------------
+*/
 var t = 3;
 //algorithms
 var fururf = [[0,true],[4,true],[3,true],[4,false],[3,false],[0,false]];
 //defs
 var colors = ["white", "yellow", "green", "blue", "red", "orange"];
-//pieces
+var axies ['x','y','z'];
+var bandFaces = [[0,3,1,2],[0,5,1,4],[4,2,5,3]];
+//bands
 var pieces = [];
-for(var i = 0; i < (t*t*t) - (t - 2); i++) {
-    pieces.push({x: 0, y: 0, z: 0, tiles: [2,3,6], type: 2});
-}
-
-function piece(n) {
-    return(pieces[n]);
-}
-
-function pieceAt(x,y,z) {
-    for (var i = 0; i < pieces.length; i++) {
-        if (pieces[i].x === x && pieces[i].y === y && pieces[i].z === z) {
-            return(pieces[i]);
-        }
+for (var l = 0; l < t; l++) {
+    for(var i = 0; i < t ; i++) {
+        pieces.push({orient: axies[l], ind: i, strips: [[bandFaces[l][0],0,0,0],[bandFaces[l][1],0,0,0],[bandFaces[l][2],0,0,0],[bandFaces[l][3],0,0,0]]});
     }
 }
-
-function rotateCorner(index,dir) {
-    if (dir === true) {
-        piece(index).tiles.splice(0,0,piece(index).tiles[piece(index).tiles.length - 1]);
-        piece(index).tiles.splice(piece(index).tiles.length - 1, 1);
-    }
-    if (dir === false) {
-        piece(index).tiles.push(piece(index).tiles.shift(1));
-    }
-}
-
 function doAlgorithm(array) {
     for (var i = 0; i < array.length; i++) {
         
